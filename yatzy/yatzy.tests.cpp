44b5f028864d9f6237c1c581b3844f6f2b17517d
Patricia Aas <psmaas@gmail.com>
2019-08-27 22:56:06 +0200

    Revert "Try to use catch"
    This reverts commit 30a64ac39a848b5f2ae6c7b9fc5c4d82b90d7209.

--- yatzy/yatzy.tests.cpp
+++ yatzy/yatzy.tests.cpp
@@ -1,51 +1,50 @@
-// For more on how to use the excellent Catch test framework see
-// https://github.com/catchorg/Catch2
-
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
 extern "C"
 {
 #include "yatzy.h"
 }
 
-TEST_CASE( "Yatzy Rules", "[hhgttg]" )
+using namespace ::testing;
+
+TEST(Hiker, Life_the_universe_and_everything)
 {
-    REQUIRE(14 == answer(Chance, 1,1,3,3,6));
-    //REQUIRE(21 == answer(Chance, 4,5,5,6,1));
+    ASSERT_EQ(14, answer(Chance, 1,1,3,3,6));
+    //ASSERT_EQ(21, answer(Chance, 4,5,5,6,1));
     
-    REQUIRE(50 == answer(Yatzy, 1,1,1,1,1));
-    REQUIRE(50 == answer(Yatzy, 5,5,5,5,5));
-    //REQUIRE(0,  answer(Yatzy, 1,1,1,2,1));
+    ASSERT_EQ(50, answer(Yatzy, 1,1,1,1,1));
+    ASSERT_EQ(50, answer(Yatzy, 5,5,5,5,5));
+    //ASSERT_EQ(0,  answer(Yatzy, 1,1,1,2,1));
     
-    REQUIRE(1 == answer(Ones, 1,3,3,4,5));
-    REQUIRE(4 == answer(Twos, 2,3,2,5,1));
-    REQUIRE(6 == answer(Threes, 2,3,3,5,1));
-    REQUIRE(8 == answer(Fours, 1,1,2,4,4));
-    REQUIRE(10 == answer(Fives, 1,1,2,5,5));
-    REQUIRE(18 == answer(Sixes, 6,6,6,1,5));
+    ASSERT_EQ(1, answer(Ones, 1,3,3,4,5));
+    ASSERT_EQ(4, answer(Twos, 2,3,2,5,1));
+    ASSERT_EQ(6, answer(Threes, 2,3,3,5,1));
+    ASSERT_EQ(8, answer(Fours, 1,1,2,4,4));
+    ASSERT_EQ(10, answer(Fives, 1,1,2,5,5));
+    ASSERT_EQ(18, answer(Sixes, 6,6,6,1,5));
     
-    REQUIRE(8 == answer(Pair, 3,3,3,4,4));
-    //REQUIRE(12 == answer(Pair, 1,1,6,2,6));
-    //REQUIRE(0 == answer(Pair, 3,3,3,4,1));
-    //REQUIRE(0 == answer(Pair, 3,3,3,3,1));
+    ASSERT_EQ(8, answer(Pair, 3,3,3,4,4));
+    //ASSERT_EQ(12, answer(Pair, 1,1,6,2,6));
+    //ASSERT_EQ(0, answer(Pair, 3,3,3,4,1));
+    //ASSERT_EQ(0, answer(Pair, 3,3,3,3,1));
     
-    REQUIRE(8 == answer(TwoPairs, 1,1,2,3,3));
-    //REQUIRE(0 == answer(TwoPairs, 1,1,2,3,4));
-    //REQUIRE(0 == answer(TwoPairs, 1,1,2,2,2));
+    ASSERT_EQ(8, answer(TwoPairs, 1,1,2,3,3));
+    //ASSERT_EQ(0, answer(TwoPairs, 1,1,2,3,4));
+    //ASSERT_EQ(0, answer(TwoPairs, 1,1,2,2,2));
     
-    REQUIRE(9 == answer(ThreeOfAKind, 3,3,3,4,5));
-    //REQUIRE(0 == answer(ThreeOfAKind, 3,3,4,5,6));
-    //REQUIRE(0 == answer(ThreeOfAKind, 3,3,3,3,1));
+    ASSERT_EQ(9, answer(ThreeOfAKind, 3,3,3,4,5));
+    //ASSERT_EQ(0, answer(ThreeOfAKind, 3,3,4,5,6));
+    //ASSERT_EQ(0, answer(ThreeOfAKind, 3,3,3,3,1));
     
-    REQUIRE(8 == answer(FourOfAKind, 2,2,2,2,5));
-    //REQUIRE(0 == answer(FourOfAKind, 2,2,2,5,5));
-    //REQUIRE(0 == answer(FourOfAKind, 2,2,2,2,2));
+    ASSERT_EQ(8, answer(FourOfAKind, 2,2,2,2,5));
+    //ASSERT_EQ(0, answer(FourOfAKind, 2,2,2,5,5));
+    //ASSERT_EQ(0, answer(FourOfAKind, 2,2,2,2,2));
     
-    REQUIRE(15 == answer(SmallStraight, 1,2,3,4,5));
+    ASSERT_EQ(15, answer(SmallStraight, 1,2,3,4,5));
     
-    REQUIRE(20 == answer(LargeStraight, 2,3,4,5,6));
+    ASSERT_EQ(20, answer(LargeStraight, 2,3,4,5,6));
     
-    REQUIRE(8 == answer(FullHouse, 1,1,2,2,2));
-    //REQUIRE(0 == answer(FullHouse, 2,2,3,3,4));
-    //REQUIRE(0 == answer(FullHouse, 4,4,4,4,4));
+    ASSERT_EQ(8, answer(FullHouse, 1,1,2,2,2));
+    //ASSERT_EQ(0, answer(FullHouse, 2,2,3,3,4));
+    //ASSERT_EQ(0, answer(FullHouse, 4,4,4,4,4));
 }
